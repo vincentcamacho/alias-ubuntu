c84dc2f801eef359d42d1791a64e1f801a908f81
Vincent Camacho <vincent.camacho.external@atos.net>
2022-03-18 10:24:29 +0100

    New

--- alias.sh
+++ alias.sh
@@ -4,17 +4,16 @@
 # OJO SELECCIONAR SHELL:  bashrc  o  zshrc
 el_shell=zshrc
 
-echo "# NECESARIOS" | sudo tee -a ~/.$el_shell
+echo -e "\n#ALIAS NECESARIOS" | sudo tee -a ~/.$el_shell
 # echo "alias cna='rm -rf alias.sh && touch alias.sh && chmod +x alias.sh && vim alias.sh'" | sudo tee -a ~/.$el_shell
-echo "alias cnz='rm -rf ~/.zshrc && cp ~/.zshrc-backup ~/.zshrc'" | sudo tee -a ~/.$el_shell
+echo "alias cnz='rm -rf ~/.zshrc && cp ~/.zshrc-backup ~/.zshrc && source /home/vcamacho/ps/alias-ubuntu/alias.sh'" | sudo tee -a ~/.$el_shell
 echo "alias bsh='sudo cat ~/.bashrc'" | sudo tee -a ~/.$el_shell
 echo "alias zsh='sudo cat ~/.zshrc'" | sudo tee -a ~/.$el_shell
 echo "alias ssha='eval \$(ssh-agent) && ssh-add'" | sudo tee -a ~/.$el_shell
 echo "alias fb='eval \$(ssh-agent) && ssh-add && ssh fac-bastion'" | sudo tee -a ~/.$el_shell
-echo "" | sudo tee -a ~/.$el_shell
 
 # Basicos
-echo "#ALIAS BASICOS" | sudo tee -a ~/.$el_shell
+echo -e "\n#ALIAS BASICOS" | sudo tee -a ~/.$el_shell
 echo "alias c='sudo cat'" | sudo tee -a ~/.$el_shell
 echo "alias t='sudo touch'" | sudo tee -a ~/.$el_shell
 echo "alias md='sudo mkdir'" | sudo tee -a ~/.$el_shell
@@ -35,17 +34,17 @@ echo "alias up1='sudo apt update -y'" | sudo tee -a ~/.$el_shell
 echo "alias up2='sudo apt update -y && sudo apt upgrade -y'" | sudo tee -a ~/.$el_shell
 
 #Sshd
-echo "#ALIAS SSHD" | sudo tee -a ~/.$el_shell
+echo -e "\n#ALIAS SSHD" | sudo tee -a ~/.$el_shell
 echo "alias sshd='sudo cat /etc/ssh/sshd_config'" | sudo tee -a ~/.$el_shell
 echo "alias sshda='cat /etc/ssh/sshd_config | grep 'PubkeyAuthentication\|PasswordAuthentication\|PermitRootLogin\|PermitEmptyPasswords''" | sudo tee -a ~/.$el_shell
 
 # Sistema
-echo "#ALIAS SISTEMA" | sudo tee -a ~/.$el_shell
+echo -e "\n#ALIAS SISTEMA" | sudo tee -a ~/.$el_shell
 echo "alias vmmc='sudo sysctl vm.max_map_count'" | sudo tee -a ~/.$el_shell
 echo "alias ffm='sudo sysctl fs.file-max'" | sudo tee -a ~/.$el_shell
 
 # Git
-echo "#ALIAS GIT" | sudo tee -a ~/.$el_shell
+echo -e "\n#ALIAS GIT" | sudo tee -a ~/.$el_shell
 echo "alias gst='git status'" | sudo tee -a ~/.$el_shell
 echo "alias gad='git add'" | sudo tee -a ~/.$el_shell
 echo "alias gaa='git add .'" | sudo tee -a ~/.$el_shell
@@ -56,7 +55,7 @@ echo "alias gpl='git pull'" | sudo tee -a ~/.$el_shell
 echo "alias gacp='git add . && git commit -m 'New changes' && git push'" | sudo tee -a ~/.$el_shell
 
 # Tmux
-echo "#ALIAS TMUX" | sudo tee -a ~/.$el_shell
+echo -e "\n#ALIAS TMUX" | sudo tee -a ~/.$el_shell
 echo "alias tml='tmux ls'" | sudo tee -a ~/.$el_shell
 echo "alias tma='tmux attach -t'" | sudo tee -a ~/.$el_shell
 echo "alias tmr='tmux rename-session -t'" | sudo tee -a ~/.$el_shell
@@ -64,14 +63,14 @@ echo "alias tmn='tmux new -s'" | sudo tee -a ~/.$el_shell
 echo "alias tmk='tmux kill-session -t'" | sudo tee -a ~/.$el_shell
 
 # IDN
-echo "#ALIAS IDN" | sudo tee -a ~/.$el_shell
+echo -e "\n#ALIAS IDN" | sudo tee -a ~/.$el_shell
 echo 'alias tprx="curl -k -x "http://proxy.dev.idnomic.com:3128" -L "https://www.amazon.com""' | sudo tee -a ~/.$el_shell
 echo 'alias proxyon="export http_proxy="http://proxy.dev.idnomic.com:3128";export https_proxy="http://proxy.dev.idnomic.com:3128""' | sudo tee -a ~/.$el_shell
 echo 'alias proxyoff="unset http_proxy;unset https_proxy"' | sudo tee -a ~/.$el_shell
 
 
 # Docker
-echo "#ALIAS DOCKER" | sudo tee -a ~/.$el_shell
+echo -e "\n#ALIAS DOCKER" | sudo tee -a ~/.$el_shell
 echo "alias d='docker'" | sudo tee -a ~/.$el_shell
 echo "alias dp='docker ps'" | sudo tee -a ~/.$el_shell
 echo "alias dpa='docker ps -a'" | sudo tee -a ~/.$el_shell
