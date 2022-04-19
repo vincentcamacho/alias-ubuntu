9c46c39e07c36704e797732b76eb19004a1193f4
Vincent <jvincent.camacho@gmail.com>
2022-04-19 13:22:34 +0200

    NewChanges

--- alias.sh
+++ alias.sh
@@ -97,7 +97,8 @@ echo "alias lsk='ls ~/.ssh'" | sudo tee -a ~/.$el_shell
 echo -e "\n#VAGRANT" | sudo tee -a ~/.$el_shell
 echo "alias vgu='vagrant up'" | sudo tee -a ~/.$el_shell
 echo "alias vgh='vagrant halt'" | sudo tee -a ~/.$el_shell
-echo "alias vgka='vagrant destroy -f && NOMBRE_VM=\${PWD##*/} && sudo rm -rf \"/mnt/c/Users/\$USUARIO_WINDOWS/VirtualBox VMs/\$NOMBRE_VM\" && sudo rm -rf .vagrant/ && SERVIDOR_IP=\$(cat Vagrantfile | grep -w \"servidor_ip = \" | cut -b 16-28) && ssh-keygen -f ~/.ssh/known_hosts -R \$SERVIDOR_IP && echo -n \"Deseas borrar la Interfaz de Red? No recomendado si tienes varias VMs creadas (n/y): \" && read RESPUESTA && if [[ \$RESPUESTA = y ]]; then echo \"Eliminando la interfaz de red de Virtual Box, el resto de la limpieza se ha realizo con EXITO\" && VBoxManage hostonlyif remove \"VirtualBox Host-Only Ethernet Adapter\" ;else echo \"La limpieza se ha realido con EXITO sin borrar la NIC de VirtualBox\"; fi'" | sudo tee -a ~/.$el_shell
+echo "alias vgka='vagrant destroy -f && NOMBRE_VM=\${PWD##*/} && sudo rm -rf \"/mnt/c/Users/\$USUARIO_WINDOWS/VirtualBox VMs/\$NOMBRE_VM\" && sudo rm -rf .vagrant/ && SERVIDOR_IP=VBoxManage guestproperty get \"\$VM_name\" \"/VirtualBox/GuestInfo/Net/1/V4/IP\" | cut -b 8-20 && ssh-keygen -f ~/.ssh/known_hosts -R \$SERVIDOR_IP && ssh-keygen -f ~/.ssh/known_hosts -R \$NOMBRE_VM && echo -n \"Deseas borrar la Interfaz de Red? No recomendado si tienes varias VMs creadas (n/y): \" && read RESPUESTA && if [[ \$RESPUESTA = y ]]; then echo \"Eliminando la interfaz de red de Virtual Box, el resto de la limpieza se ha realizo con EXITO\" && VBoxManage hostonlyif remove \"VirtualBox Host-Only Ethernet Adapter\" ;else echo \"La limpieza se ha realido con EXITO sin borrar la NIC de VirtualBox\"; fi'" | sudo tee -a ~/.$el_shell
+
 
 
 
